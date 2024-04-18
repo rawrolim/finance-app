8aeeea155724863be463596647b73da3d5b6444a
Rawlinson Rolim <alemaorolim@gmail.com>
2024-04-18 16:58:05 -0300

    Teste de cors

---
+++ config/http.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const http = axios.create({
+    headers: {
+        empresa_id: process.env.AUTH_EMPRESA_ID,
+        app_id: process.env.AUTH_APP_ID,
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    },
+    baseURL: process.env.AUTH_URL
+});
+
+console.log("TESTEEE")
+export default http;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    AUTH_URL: process.env.AUTH_URL,
+    AUTH_EMPRESA_ID: process.env.AUTH_EMPRESA_ID,
+    AUTH_APP_ID: process.env.AUTH_APP_ID,
+  }
 }
 
 module.exports = nextConfig
