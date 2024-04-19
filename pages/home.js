c1809d01d9c61ed940e1d59e75f03f8275e07b55
Rawlinson Rolim <alemaorolim@gmail.com>
2024-04-19 15:21:09 -0300

    DONE

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import httpInternal from '../config/httpInternal'
 
-export default function handler() {
+export default function Handler() {
     useEffect(async ()=>{
         await httpInternal.get("teste")
     },[])
