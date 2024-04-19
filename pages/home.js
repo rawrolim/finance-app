bcf60460bed052aea9db304f2c2ab22bab1fb462
Rawlinson Rolim <alemaorolim@gmail.com>
2024-04-19 16:05:25 -0300

    DONE

--- pages/home.js
+++ pages/home.js
@@ -3,11 +3,18 @@ import httpInternal from '../config/httpInternal'
 
 export default function Handler() {
     useEffect(async ()=>{
+        await send()
+    },[]);
+
+    async function send(){
         await httpInternal.get("teste")
-    },[])
+    }
+
     return (
         <div>
-            
+            <button onClick={send}> 
+                Testar
+            </button>
         </div>
     )
 }
